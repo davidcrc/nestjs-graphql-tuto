2802f52a122f942f8ad431772a5982fafc45f647
davidcrc <david_drcr@hotmail.com>
2023-09-28 09:19:33 -0500

    feat: :fire: update dependencis and add graphql

--- INSTALL.md
+++ INSTALL.md
@@ -11,3 +11,13 @@ nest g service posts
 ```bash
 nest g resolver posts
 ```
+
+## Add graphql - CHECK UPDATE all dependencies in packgage.json
+
+- https://docs.nestjs.com/graphql/quick-start
+
+```bash
+pnpm i @nestjs/graphql @nestjs/apollo @apollo/server graphql
+```
+
+- WE can use code first or Schema first, it depends from project (code first )
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
